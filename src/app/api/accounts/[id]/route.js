28d35ca9c2b41473a59b57de61bafdf7c0ec1118
Steven Qie <steven.qie@gmail.com>
2025-03-01 14:36:00 -0600

    api done

--- src/app/api/accounts/[id]/route.js
+++ src/app/api/accounts/[id]/route.js
@@ -4,7 +4,6 @@ const API_KEY = "309a844675e8f5c82d723fe65a6ae49d";
 const API_BASE_URL = "http://api.nessieisreal.com";
 
 export async function GET(request, {params}) {
-
     try {
         const res = await fetch(`${API_BASE_URL}/accounts/${params.id}?key=${API_KEY}`);
         const data = await res.json();
