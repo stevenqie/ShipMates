ed6ff12b25724b73ceb5072325e340cc62a93fd8
JCalago02 <jcalago02@gmail.com>
2025-03-02 00:14:40 -0600

    Add home page enhancements

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,22 @@
-import Image from "next/image";
-
+import { Box, Button, Input, VStack, Heading, Text, Image, Container} from "@chakra-ui/react";
+import HomePageResponsive from "@/components/HomePageResponsive";
 export default function Home() {
+  const bgColor = "gray.50"; // Light & dark mode support
+
   return (
-    <h1> Hello World </h1>
+    <Box minH="100vh" bg={bgColor} display="flex" alignItems="center" justifyContent="center">
+      <Container maxW="lg" textAlign="center" py={10}>
+        {/* App Logo */}
+        <Image src="/logo.png" alt="What's in Your Cart?" boxSize="100px" mx="auto" mb={4} />
+
+        {/* Title and Description */}
+        <Heading size="xl" color="blue.600">What's in Your Cart?</Heading>
+        <Text fontSize="lg" mt={3} color="gray.600">
+          Powered by Capital One. Find the best deals near you.
+        </Text>
+        <HomePageResponsive/>
+
+      </Container>
+    </Box>
   );
 }
